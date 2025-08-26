cf30b02427b05136beed1bb50e84085bfe007e0c
Tinendrakumar <tinendrakumar@gmail.com>
2025-08-25 23:14:12 -0400

    Publish built site

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: 'Tinendrakumar.github.io', // Change this to your GitHub repo name
+  base: 'tinendrakumar.github.io', // Change this to your GitHub repo name
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
